cfd9f1a505228f56602d2929c73fb49c7dc3e619
Brian T Bailey <briantbailey@gmail.com>
2022-10-25 22:53:13 -0500

    Added Week 10 Files

--- week9/intersectionobserver/io-test.js
+++ week9/intersectionobserver/io-test.js
@@ -1,13 +1,13 @@
 let options = {
     root: null,
     rootMargin: '0px',
-    threshold: .25
+    threshold: [0, 0.25, 0.5, 0.75, 1]
 };
 
 let callback = (entries, observer) => {
     entries.forEach(entry => {
         console.log(entry);
-        if (entry.intersectionRatio > .25) {
+        if (entry.intersectionRatio >= .5) {
             entry.target.classList.add('active');
             //observer.unobserve(target);
         } else {
