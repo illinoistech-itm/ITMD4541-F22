3782127d54327eca954eb9c53daba09d6cd6e301
Brian T Bailey <briantbailey@gmail.com>
2022-11-15 23:20:01 -0600

    added week 13 demos

---
+++ week13/webpack-demo/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Headline from './Headline';
+
+class App extends React.Component {
+    render(){
+        return (
+            <Headline text="Hello World2!" />
+        );
+    }
+}
+
+export default App;
